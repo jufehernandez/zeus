f38242c1d7f8f139a9cbf0fdf6e294fe2a77726a
Ferodoto <ferodoto@vk.com>
2026-03-05 13:37:47 -0500

    Cambios hechos en clase 05/03/26

--- AppPagina/models.py
+++ AppPagina/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiantes (models.Model): 
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    apellido = models.CharField(max_length=50, verbose_name="Apellido")
+    foto = models.ImageField(upload_to='fotos/', verbose_name="Foto", null=True)
+    clase = models.TextField(verbose_name="Clase", null=True)
+    def __str__(self):
+        return self.nombre +" " + self.apellido
