4ae0200bdfa98ef8547b7f42b88ab377f6acedd7
jufehernandez <jufehernandez@soy.sena.edu.co>
2026-03-10 12:53:37 -0500

    Cambios hechos en clase 10/03/26
    Intento de editar y borrar estudiantes

--- AppPagina/views.py
+++ AppPagina/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Estudiantes
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EstudiantesForm
 
 # Create your views here.
@@ -33,10 +33,28 @@ def crear(request):
     formulario = EstudiantesForm(request.POST or None, request.FILES or None)
     if formulario.is_valid():
         formulario.save()
-    return render(request, 'estudiantes/crear.html', {'formulario':formulario})
+        return redirect('estudiantes')
+    return render(request, 'estudiantes/crear.html', {'formulario': formulario})
 
-def editar(request):
-    return render(request, 'estudiantes/editar.html')
+def editar(request, id):
+    estudianteE = get_object_or_404(Estudiantes, id=id)
+
+    if request.method == "POST":
+        formulario = EstudiantesForm(request.POST, request.FILES, instance=estudianteE)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('estudiantes')
+    else:
+        formulario = EstudiantesForm(instance=estudianteE)
+
+    return render(request, 'estudiantes/editar.html', {'formulario': formulario})
+
 
 def formulario(request):
     return render(request, 'estudiantes/formulario.html')
+
+def eliminar(request, id):
+    estudianteD = get_object_or_404(Estudiantes, id=id)
+    if request.method == "POST":
+        estudianteD.delete()
+    return redirect('estudiantes')
